8da3c4c1310d8b2a897dbd4cb1b38481d2e3a853
PriyaRajarathinam <35835697+PriyaRajarathinam@users.noreply.github.com>
2019-03-24 17:26:40 -0700

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -34,7 +34,7 @@ def create_playlist_from_favorites(token):
 
 
 if __name__ == "__main__":
-    cmds=["export SPOTIPY_CLIENT_ID='0ceb01c560f34814ad5b5ae57adc0f0b'","export SPOTIPY_CLIENT_SECRET='f725256974624e968cb191cade4acd43'","export SPOTIPY_REDIRECT_URI='http://localhost:8888/callback/'"]
+   
     for cmd in cmds:
         print(cmd)
         os.system(cmd)
